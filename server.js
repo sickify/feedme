3b81b5f9f0df679e480cd1871bea9d8699777375
Battenfield <chrisbattenfield@gmail.com>
2016-10-25 23:35:54 -0700

    <sickify>[init](server): Setup twitter module for incoming tweets {rad}

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+var CKEY = process.env.TWITTER_KEY;
+var CSECRET = process.env.TWITTER_SECRET;
+var ATOKEN = process.env.TWITTER_ACCESS_TOKEN;
+var ASECRET = process.env.TWITTER_ACCESS_SECRET
 var express = require('express');
+var Twit = require('twit');
+
 var app = express();
+
+var T = new Twit({
+  consumer_key:       CKEY,
+  consumer_secret:    CSECRET,
+  access_token:       ATOKEN,
+  access_token_secret:ASECRET
+})
 
 app.use(express.static('public'));
 
